7f0814a988dfe9b7f8f4c64a383d16f5d7f971d5
farahagia <farragiafrh@gmail.com>
2017-10-21 14:07:22 +0700

    add

---
+++ APAPTutorial6/src/main/java/com/example/ApapTutorial6Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial6Application
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (ApapTutorial6Application.class, args);
+    }
+}
